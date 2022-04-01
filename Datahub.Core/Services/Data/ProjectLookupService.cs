d76bf3df8af716d97e675ba1f454a70ec0f97896
gcerik <82101285+gcerik@users.noreply.github.com>
2022-04-01 17:22:41 -0400

    initial updates  with multiple storage containers

---
+++ Datahub.Core/Services/Data/ProjectLookupService.cs
@@ -0,0 +1,20 @@
+﻿using Datahub.Core.EFCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahub.Core.Services.Data
+{
+    public class ProjectLookupService
+    {
+        private readonly IDbContextFactory<DatahubProjectDBContext> dbContextFactory;
+
+        public ProjectLookupService(IDbContextFactory<DatahubProjectDBContext> dbContextFactory)
+        {
+            this.dbContextFactory = dbContextFactory;
+        }
+    }
+}
