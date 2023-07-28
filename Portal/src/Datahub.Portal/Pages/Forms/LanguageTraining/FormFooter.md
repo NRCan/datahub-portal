5e1c3d76e1c3489bd45687e5ba4db0480886e482
gcnabeel <83655937+gcnabeel@users.noreply.github.com>
2023-07-28 00:50:48 -0400

    language aug 2023 changes

--- Portal/src/Datahub.Portal/Pages/Forms/LanguageTraining/FormFooter.md
+++ Portal/src/Datahub.Portal/Pages/Forms/LanguageTraining/FormFooter.md
@@ -1,11 +1,9 @@
 ﻿### Commitment
 
+_**Reminder**_
+
 _By submitting this application, I confirm that:_
-
 _- I have had a discussion with my supervisor/manager about language training and it is part of my learning plan;_
-
-_- I commit to attend the first two weeks of the session should I be accepted;_ 
-
-_- I commit to the language training plan and will not miss more than 20% of classes;_ 
-
+_- I commit to attend the first two weeks of the session should I be accepted;_
+_- I commit to the language training plan and will not miss more than 20% of classes;_
 _- I commit to using my second official language in the workplace and maintaining my level once my objectives are reached._
--- Portal/src/modules/Datahub.LanguageTraining/Data/Language_Constants.cs
+++ Portal/src/modules/Datahub.LanguageTraining/Data/Language_Constants.cs
@@ -29,25 +29,44 @@ public class Language_Constants
 
     public static readonly string[] QUARTERS = { "Q1 - April to June", "Q2 - July to September", "Q3 – October to December", "Q4 – January to March" };
 
-    public static readonly string[] TRAININGTYPEFRENCH = { "Group Training - Part-Time Training" };
+    public static readonly string[] TRAININGTYPEFRENCH = { "Group Training - Part-Time Training", "Group Training – Full-time Training (French program only)" };
     public static readonly string[] TRAININGTYPEENGLISH = { "Group training : Part-time (6 hours of class + 2 hours self-learning)", "Group training : Part-time (3 hours of class + 1 hour self-learning)", "Group training : Part-time (1.5 hours of class + 0 hours self-learning)*", "Private training: Coaching**" };
 
     public static readonly string[] CLASSES_E = { "Beginner 1",
         "Beginner 2",
         "Beginner 3",
-        "Intermediate 1 (BBB)",
-        "Intermediate 2 (BBB)",
-        "Intermediate 3 (BBB)",
-        "Intermediate 4 (BBB)",
-        "Intermediate 5 (BBB)",
-        "Intermediate 6 (BBB)",
-        "Intermediate 7 – Test Oral Proficiency – B level",
-        "Advanced 1 (CBC)",
-        "Advanced 2 (CBC)",
-        "Advanced 3 (CBC)",
-        "Advanced 4 (CBC)",
-        "Advanced 5 (CBC)",
-        "Advanced 6 – Test Oral Proficiency – C level"
+        "Intermediate 1",
+        "Intermediate 2",
+        "Intermediate 3",
+        "Intermediate 4",
+        "Intermediate 5",
+        "Intermediate 6",
+        "Advanced 1",
+        "Advanced 2",
+        "Advanced 3",
+        "Advanced 4",
+        "Advanced 5",
+        "Preparation C"
+    };
+
+    public static readonly string[] CLASSES_E_FRENCH = { "Débutant 1",
+        "Débutant 2",
+        "Débutant 3",
+        "Intermédiaire 1 (BBB)",
+        "Intermédiaire 2 (BBB)",
+        "Intermédiaire 3 (BBB)",
+        "Intermédiaire 4 (BBB)",
+        "Intermédiaire 5 (BBB)",
+        "Intermédiaire 6 (BBB)",
+        "Préparation B",
+        "Maintient B",
+        "Enrichi 1 (CBC)",
+        "Enrichi 2 (CBC)",
+        "Enrichi 3 (CBC)",
+        "Enrichi 4 (CBC)",
+        "Enrichi 5 (CBC)",
+        "Préparation C",
+        "Maintient C"
     };
 
     public static readonly string[] CLASSES_F = {   "Intermediate 1 (BBB)",
